e4d991b6e7472154c9c8e2fe8b2b5e63b631e0bf
Kratos1top <108674440+Kratos1top@users.noreply.github.com>
2022-09-19 15:03:27 +0600

    Delete errors, create env file

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -30,7 +30,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/kratos/Desktop/Shop_project/venv"
+set -gx VIRTUAL_ENV "/home/kratos/Desktop/Shop/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
